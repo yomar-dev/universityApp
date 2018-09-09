0469f8cf59abcb680e3b8ab807e5958b99807e8d
Yomar Miranda <mirandayomar@gmail.com>
2018-09-09 10:23:17 -0500

    Add  angular-route.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('universityApp',[ ]);
+var app = angular.module('universityApp',['ngRoute']);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
     $scope.navbar = 'partials/nav.html';
